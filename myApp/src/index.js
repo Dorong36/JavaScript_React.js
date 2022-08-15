f880cbd67fb9a361e612672bd8d4cd63d3f54ffa
유도영 <dbehdud35@naver.com>
2022-08-15 15:43:14 +0900

    0814

--- myApp/src/index.js
+++ myApp/src/index.js
@@ -4,15 +4,33 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import Library from './ch03/Library';  // Library 컴포넌트 import 해주기
+// import Library from './ch03/Library';
+import Clock from './ch04/Clock';
 
+// ch04 예제
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    {/* App ==> Library 수정 */}
-    <Library />   
-  </React.StrictMode>
-);
+setInterval(()=>{
+  root.render(
+    <React.StrictMode>
+      <Clock />   
+    </React.StrictMode>
+  );
+}, 1000);
+// 🌟 참고사항!!
+// 과거 ReactDom.render(React 엘리먼트, 실제 DOM 엘리먼트) 문법이 아래 두 줄로 바뀜!!
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(React 엘리먼트)
+
+
+// ch03 예제
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     {/* App ==> Library 수정 */}
+//     <Library />   
+//   </React.StrictMode>
+// );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
